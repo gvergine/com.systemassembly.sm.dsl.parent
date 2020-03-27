fea4563f1dc3971b336fb45d7a915c8efb1f59ce
Giovanni Vergine <verginegiovanni@gmail.com>
2020-03-27 18:02:50 +0200

    - Simplified grammar, removing "states {}" section [Breaking change!] - Added validators to check that initial state is one and only one

--- com.systemassembly.sm.dsl.web/WebRoot/xtext-resources/generated/mode-sm.js
+++ com.systemassembly.sm.dsl.web/WebRoot/xtext-resources/generated/mode-sm.js
@@ -1,6 +1,6 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "commands|entry|events|exec|exit|goto|initial|name|on|state|states";
+		var keywords = "commands|entry|events|exec|exit|goto|initial|name|on|state";
 		this.$rules = {
 			"start": [
 				{token: "comment", regex: "\\/\\/.*$"},
